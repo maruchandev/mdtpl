48f0996bb93c34b0d6e22dfd78df83f1ff0b2fef
maruchan <k24rs122@gmail.com>
2025-05-06 21:43:59 +0900

    Update setup.cmd

--- setup.cmd
+++ setup.cmd
@@ -2,4 +2,4 @@
 SET /P "name=Workspace name please: "
 curl -o "%name%.code-workspace" https://md.maruchan.dev/tpl.code-workspace
 
-curl https://md.maruchan.dev/tpllist.json
+curl https://md.maruchan.dev/list.json
