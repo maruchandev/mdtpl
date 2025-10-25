3b5ff16ef80c79248ab73e4d67f2cc31d66e9553
TaikiMaruyama <k24rs122@gmail.com>
2025-10-26 08:29:39 +0900

    Update setup.cmd

--- setup.cmd
+++ setup.cmd
@@ -2,6 +2,9 @@
 SET /P "name=Workspace name please: "
 curl -o "%name%.code-workspace" https://md.maruchan.dev/tpl.code-workspace
 
+echo [InternetShortcut] > "%name% - MaruchanDev.url"
+echo URL=https://md.maruchan.dev/ >> "%name% - MaruchanDev.url"
+
 start https://md.maruchan.dev/
 open "%name%.code-workspace"
 
